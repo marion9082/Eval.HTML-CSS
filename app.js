c9a662f70a0352ffe9645d3aa88a6b7cd5364144
Marion C <marion.chedal.bornu@gmail.com>
2023-02-24 08:15:30 +0100

    add js file

---
+++ app.js
@@ -0,0 +1,11 @@
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll',  function(event) {
+    event.preventDefault();
+
+    if (window.scrollY > 20) {
+        navbar.classList.add('nav-height');
+    } else {
+        navbar.classList.remove('nav-height');
+    }
+})
